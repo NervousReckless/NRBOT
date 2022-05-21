3ae7d9c5042b884f578110825260c154d4ae0252
NervousReckless <46354742+NervousReckless@users.noreply.github.com>
2022-05-21 17:52:10 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+const { default: SpotifyPlugin } = require("@distube/spotify");
+const { Client, Collection } = require("discord.js");
+require('dotenv').config();
+
+const client = new Client({
+  intents: ["GUILDS", "GUILD_MESSAGES", "DIRECT_MESSAGES", "GUILD_VOICE_STATES"],
+  partials: ["MESSAGE", "CHANNEL", "REACTION"]
+});
+
+client.commands = new Collection();
+
+const{ DisTube } = require("distube");
+//const { SpotifyPlugin } = require("@distube/spotify");
+
+client.distube = new DisTube(client, {
+    emitNewSongOnly: true,
+    leaveOnFinish: true,
+    emitAddListWhenCreatingQueue: false,
+    plugins: [new SpotifyPlugin()]
+});
+module.exports = client;
+
+require("./Handlers/Events")(client);
+require("./Handlers/Commands")(client);
+
+
+
+client.login(process.env.DISCORD_TOKEN);
